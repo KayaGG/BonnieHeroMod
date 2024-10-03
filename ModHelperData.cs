fc84d49257705f2234319b6cc938201056f0cb15
KayaGG <falling10skys@gmail.com>
2024-10-02 22:10:32 -0700

    Updated ModHelperData

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,11 +3,11 @@ namespace BonnieHeroMod;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "44.0";
-    public const string Version = "1.0.0";
+    public const string Version = "44.0-beta.1";
     public const string Name = "BonnieHeroMod";
 
-    public const string Description = "An empty mod";
+    public const string Description = "A project aiming to port Bonnie from BTD Battles 2 over to BTD 6 and make rebalance her for the different environment. Made with the help of GrahamKracker. \n KNOWN BUGS: \n Embrittlement / Super Brittle cause BEAST to not drop full value. \n UI Initialization fails under certain circumstances. \nPlease still report both of these WITH DETAILS";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "KayaGG"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "BonnieHeroMod"; // TODO add your repo name here, also in the download url in README.md
 }
